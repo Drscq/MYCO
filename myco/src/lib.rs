e535ad3d1039e99b7b60fac12f3cf956d5989155
Darya Kaviani <daryakavi@gmail.com>
2025-04-18 22:06:59 -0700

    Initial Commit

---
+++ myco/src/lib.rs
@@ -0,0 +1,22 @@
+//! # Myco
+
+//! "Myco" is a Rust library that enhances user anonymity in encrypted messaging by hiding metadata 
+//! like communication timing and participant relationships. It uses an innovative data structure 
+//! inspired by ORAM to achieve efficient read and write operations, while maintaining strong 
+//! cryptographic guarantees. By separating message writing and reading across two servers, Myco 
+//! significantly improves performance compared to existing systems.
+
+// Add module declarations
+pub mod constants;
+pub mod dtypes;
+pub mod utils;
+pub mod network;
+pub mod server1;
+pub mod server2;
+pub mod tree;
+pub mod client;
+pub mod logging;
+pub mod proto;
+pub mod crypto;
+pub mod error;
+pub mod megaclient;
